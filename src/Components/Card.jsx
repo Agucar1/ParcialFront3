7fa2617a5200618da614230466a0f2a4fe9a57dd
Agucar1 <agucar19@gmail.com>
2024-08-15 22:47:11 -0300

    entrega 2247

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Card = ({ name, car }) => {
   return (
     <div className="card">
-      <h2>Ahora sabemos {name} que tu auto favorito es un {car}</h2>
+      <h2>Hola {name}!</h2>
+      <h2>Ahora sabemos que tu auto favorito es un: </h2>
+      <h2>!{car}!🚗</h2>
     </div>
   );
 };
